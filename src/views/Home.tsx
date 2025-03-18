f511d29f3da2835023710f964bbafcb44ef4a95b
Matheus <matheus.carvalho@unlv.edu>
2025-03-18 11:27:53 -0700

    add-lib

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,7 +3,7 @@ import { Image, Text, View } from "react-native";
 export function Home() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
+
 
       <Text style={{ marginTop: 20 }}>Welcome to the Home Screen!</Text>
     </View>
